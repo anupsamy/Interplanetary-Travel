48af5ea98c1f7513e3c5f7e5bee23923363a7cb0
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2022-10-17 14:40:35 -0700

    Initialized repo for MP2

---
+++ src/main/java/cpen221/mp2/graph/AMGraph.java
@@ -0,0 +1,13 @@
+package cpen221.mp2.graph;
+
+public class AMGraph<V extends Vertex, E extends Edge<V>> implements MGraph<V, E> {
+
+    /**
+     * Create an empty graph with an upper-bound on the number of vertices
+     * @param maxVertices is greater than 1
+     */
+    public AMGraph(int maxVertices) {
+        // TODO: Implement this method
+    }
+
+}
