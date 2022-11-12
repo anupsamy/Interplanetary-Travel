52ef6129c9b897685c3890d803797c8cba7c0baa
Anu <anumithrap@gmail.com>
2022-11-11 16:13:44 -0800

    added pathLength method beepboopblurp

--- src/main/java/cpen221/mp2/graph/Graph.java
+++ src/main/java/cpen221/mp2/graph/Graph.java
@@ -22,6 +22,152 @@ public class Graph<V extends Vertex, E extends Edge<V>> extends ALGraph<V,E> imp
         graph = new ALGraph<Vertex, Edge<Vertex>>();
     }
 
+    /**
+     * Find the edge that connects two vertices if such an edge exists.
+     * This method should not permit graph mutations.
+     *
+     * @param v1 one end of the edge
+     * @param v2 the other end of the edge
+     * @return the edge connecting v1 and v2
+     */
+//    @Override
+//    public E getEdge(V v1, V v2) {
+//        E toReturn = null;
+//        var copy = new HashMap<>(getMap());
+//        for (E e : copy.get(v1)) {
+//            if (e.v1().equals(v2) || e.v2().equals(v2)) {
+//                toReturn = e;
+//            }
+//        }
+//        return toReturn;
+//    }
+    @Override
+    public E getEdge(V v1, V v2) {
+
+        //Get all edges in graph
+        Set<E> edgeSet = this.allEdges();
+
+        //Check whether edge is in set
+        for(E edge : edgeSet) {
+            if(edge.v1().equals(v1) && edge.v2().equals(v2)) {
+                return (E) edge.clone();
+            }
+            else if(edge.v1().equals(v2) && edge.v2().equals(v1)) {
+                return (E) edge.clone();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Compute the shortest path from source to sink
+     *
+     * @param source the start vertex
+     * @param sink   the end vertex
+     * @return the vertices, in order, on the shortest path from source to sink (both end points are part of the list)
+     */
+    @Override
+    public List<V> shortestPath(V source, V sink) {
+        List<V> vertices = new ArrayList<>();
+        return null;
+    }
+
+    /**
+     * Compute the length of a given path
+     *
+     * @param path indicates the vertices on the given path
+     * @return the length of path
+     */
+    @Override
+    public int pathLength(List<V> path) {
+        int length = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            length += getEdge(path.get(i), path.get(i + 1)).length();
+        }
+        return length;
+    }
+
+    /**
+     * Obtain all vertices w that are no more than a <em>path distance</em> of range from v.
+     *
+     * @param v     the vertex to start the search from.
+     * @param range the radius of the search.
+     * @return a map where the keys are the vertices in the neighbourhood of v,
+     * and the value for key w is the last edge on the shortest path
+     * from v to w.
+     */
+    @Override
+    public Map<V, E> getNeighbours(V v, int range) {
+        Map<V, E> neighbours = new HashMap<>();
+        return neighbours;
+    }
+
+    /**
+     * Return a set with k connected components of the graph.
+     *
+     * <ul>
+     * <li>When k = 1, the method returns one graph in the set, and that graph
+     * represents the minimum spanning tree of the graph.
+     * See: https://en.wikipedia.org/wiki/Minimum_spanning_tree</li>
+     *
+     * <li>When k = n, where n is the number of vertices in the graph, then
+     * the method returns a set of n graphs, and each graph contains a
+     * unique vertex and no edge.</li>
+     *
+     * <li>When k is in [2, n-1], the method partitions the graph into sub-graphs
+     * such that for any two vertices V_i and V_j, if vertex V_i is in subgraph
+     * G_a and vertex V_j is in subgraph G_b (a != b), and there is an edge
+     * between V_i and V_j then there must exist some vertex V_k in G_a such
+     * that the length of the edge between V_i and V_k is at most the length
+     * of the edge between V_i and V_j.</li>
+     * </ul>
+     *
+     * @param k
+     * @return a set of graph partitions such that a vertex in one partition
+     * is no closer to a vertex in a different partition than it is to a vertex
+     * in its own partition.
+     */
+    @Override
+    public Set<ImGraph<V, E>> minimumSpanningComponents(int k) {
+        return null;
+    }
+
+    /**
+     * Compute the diameter of the graph.
+     * <ul>
+     * <li>The diameter of a graph is the length of the longest shortest path in the graph.</li>
+     * <li>If a graph has multiple components then we will define the diameter
+     * as the diameter of the largest component.</li>
+     * </ul>
+     *
+     * @return the diameter of the graph.
+     */
+    @Override
+    public int diameter() {
+        return 0;
+    }
+
+    /**
+     * Compute the center of the graph.
+     *
+     * <ul>
+     * <li>For a vertex s, the eccentricity of s is defined as the maximum distance
+     * between s and any other vertex t in the graph.</li>
+     *
+     * <li>The center of a graph is the vertex with minimum eccentricity.</li>
+     *
+     * <li>If a graph is not connected, we will define the graph's center to be the
+     * center of the largest connected component.</li>
+     * </ul>
+     *
+     * @return the center of the graph.
+     */
+    @Override
+    public V getCenter() {
+        return null;
+    }
+
     //// add all new code above this line ////
 
     /**
@@ -89,144 +235,4 @@ public class Graph<V extends Vertex, E extends Edge<V>> extends ALGraph<V,E> imp
         }
     }
 
-    /**
-     * Find the edge that connects two vertices if such an edge exists.
-     * This method should not permit graph mutations.
-     *
-     * @param v1 one end of the edge
-     * @param v2 the other end of the edge
-     * @return the edge connecting v1 and v2
-     */
-//    @Override
-//    public E getEdge(V v1, V v2) {
-//        E toReturn = null;
-//        var copy = new HashMap<>(getMap());
-//        for (E e : copy.get(v1)) {
-//            if (e.v1().equals(v2) || e.v2().equals(v2)) {
-//                toReturn = e;
-//            }
-//        }
-//        return toReturn;
-//    }
-    @Override
-    public E getEdge(V v1, V v2) {
-
-        //Get all edges in graph
-        Set<E> edgeSet = this.allEdges();
-
-        //Check whether edge is in set
-        for(E edge : edgeSet) {
-            if(edge.v1().equals(v1) && edge.v2().equals(v2)) {
-                return (E) edge.clone();
-            }
-            else if(edge.v1().equals(v2) && edge.v2().equals(v1)) {
-                return (E) edge.clone();
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Compute the shortest path from source to sink
-     *
-     * @param source the start vertex
-     * @param sink   the end vertex
-     * @return the vertices, in order, on the shortest path from source to sink (both end points are part of the list)
-     */
-    @Override
-    public List<V> shortestPath(V source, V sink) {
-
-        return null;
-    }
-
-    /**
-     * Compute the length of a given path
-     *
-     * @param path indicates the vertices on the given path
-     * @return the length of path
-     */
-    @Override
-    public int pathLength(List<V> path) {
-        return 0;
-    }
-
-    /**
-     * Obtain all vertices w that are no more than a <em>path distance</em> of range from v.
-     *
-     * @param v     the vertex to start the search from.
-     * @param range the radius of the search.
-     * @return a map where the keys are the vertices in the neighbourhood of v,
-     * and the value for key w is the last edge on the shortest path
-     * from v to w.
-     */
-    @Override
-    public Map<V, E> getNeighbours(V v, int range) {
-        return null;
-    }
-
-    /**
-     * Return a set with k connected components of the graph.
-     *
-     * <ul>
-     * <li>When k = 1, the method returns one graph in the set, and that graph
-     * represents the minimum spanning tree of the graph.
-     * See: https://en.wikipedia.org/wiki/Minimum_spanning_tree</li>
-     *
-     * <li>When k = n, where n is the number of vertices in the graph, then
-     * the method returns a set of n graphs, and each graph contains a
-     * unique vertex and no edge.</li>
-     *
-     * <li>When k is in [2, n-1], the method partitions the graph into sub-graphs
-     * such that for any two vertices V_i and V_j, if vertex V_i is in subgraph
-     * G_a and vertex V_j is in subgraph G_b (a != b), and there is an edge
-     * between V_i and V_j then there must exist some vertex V_k in G_a such
-     * that the length of the edge between V_i and V_k is at most the length
-     * of the edge between V_i and V_j.</li>
-     * </ul>
-     *
-     * @param k
-     * @return a set of graph partitions such that a vertex in one partition
-     * is no closer to a vertex in a different partition than it is to a vertex
-     * in its own partition.
-     */
-    @Override
-    public Set<ImGraph<V, E>> minimumSpanningComponents(int k) {
-        return null;
-    }
-
-    /**
-     * Compute the diameter of the graph.
-     * <ul>
-     * <li>The diameter of a graph is the length of the longest shortest path in the graph.</li>
-     * <li>If a graph has multiple components then we will define the diameter
-     * as the diameter of the largest component.</li>
-     * </ul>
-     *
-     * @return the diameter of the graph.
-     */
-    @Override
-    public int diameter() {
-        return 0;
-    }
-
-    /**
-     * Compute the center of the graph.
-     *
-     * <ul>
-     * <li>For a vertex s, the eccentricity of s is defined as the maximum distance
-     * between s and any other vertex t in the graph.</li>
-     *
-     * <li>The center of a graph is the vertex with minimum eccentricity.</li>
-     *
-     * <li>If a graph is not connected, we will define the graph's center to be the
-     * center of the largest connected component.</li>
-     * </ul>
-     *
-     * @return the center of the graph.
-     */
-    @Override
-    public V getCenter() {
-        return null;
-    }
 }
